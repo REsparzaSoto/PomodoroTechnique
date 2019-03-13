e33662de67d443435e3d794dc589264e73d30326
Rubén Esparza <rubenesparzasoto95@gmail.com>
2019-03-12 23:17:22 -0700

    Se sube la aplicación Pomodoro realizada en Xamarin.Forms

---
+++ Pomodoro/Pomodoro/Pomodoro/Literals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pomodoro
+{
+    internal class Literals
+    {
+        public static string PomodoroDuration = "PomodoroDuration";
+        public static string BreakDuration = "BreakDuration";
+        public static string History = "History";
+    }
+}
---
+++ Pomodoro/Pomodoro/Pomodoro/RootPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Pomodoro.ViewModels;
+using Pomodoro.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pomodoro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RootPage : MasterDetailPage
+    {
+        public RootPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<RootPageViewModel>(this, "GoToPomodoro", (a) =>
+            {
+                Detail = new NavigationPage(new PomodoroPage());
+                IsPresented = false;
+            });
+
+            MessagingCenter.Subscribe<RootPageViewModel>(this, "GoToHistory", (a) =>
+            {
+                Detail = new NavigationPage(new HistoryPage());
+                IsPresented = false;
+            });
+
+            MessagingCenter.Subscribe<RootPageViewModel>(this, "GoToConfiguration", (a) =>
+            {
+                Detail = new NavigationPage(new ConfigurationPage());
+                IsPresented = false;
+            });
+        }
+    }
+}
